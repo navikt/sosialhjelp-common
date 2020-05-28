30b7301e6632e84545b33dbedb2c9acfa72b9a96
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-05-28 10:50:13 +0200

    publisere snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 object Versions {
-    const val coroutines = "1.3.7"
-
     // Test only
     const val junitJupiter = "5.6.0"
     const val kluent = "1.61"
@@ -42,8 +40,6 @@ subprojects {
     dependencies {
         implementation(kotlin("stdlib"))
         implementation(kotlin("reflect"))
-
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
 
 //        Test
         testImplementation("org.junit.jupiter:junit-jupiter:${Versions.junitJupiter}")
--- sosialhjelp-common-selftest/build.gradle.kts
+++ sosialhjelp-common-selftest/build.gradle.kts
@@ -1,10 +1,53 @@
 object Versions {
+    const val coroutines = "1.3.7"
     const val jackson = "2.11.0"
 }
 
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
 dependencies {
+//    Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
+
 //    Jackson
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}")
 
 }
 
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/sosialhjelp-common")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            pom {
+                name.set("sosialhjelp-common-selftest")
+                description.set("Bibliotek for selftest i sosialhjelp-domene")
+                url.set("https://github.com/navikt/sosialhjelp-common")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/navikt/sosialhjelp-common.git")
+                    developerConnection.set("scm:git:https://github.com/navikt/sosialhjelp-common.git")
+                    url.set("https://github.com/navikt/sosialhjelp-common")
+                }
+            }
+            from(components["java"])
+        }
+    }
+}
