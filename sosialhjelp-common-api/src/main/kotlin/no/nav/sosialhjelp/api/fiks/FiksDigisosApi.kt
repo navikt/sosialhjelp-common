48109f149d04be4f18fc671ba197ad5b48062688
martintveter <41987225+martintveter@users.noreply.github.com>
2020-06-10 10:52:54 +0200

    Common api (#4)
    * ny module med felles api-modeller. I første omgang FiksDigisosApi

---
+++ sosialhjelp-common-api/src/main/kotlin/no/nav/sosialhjelp/api/fiks/FiksDigisosApi.kt
@@ -0,0 +1,77 @@
+package no.nav.sosialhjelp.api.fiks
+
+data class DigisosSak(
+        val fiksDigisosId: String,
+        val sokerFnr: String,
+        val fiksOrgId: String,
+        val kommunenummer: String,
+        val sistEndret: Long,
+        val originalSoknadNAV: OriginalSoknadNAV?,
+        val ettersendtInfoNAV: EttersendtInfoNAV?,
+        val digisosSoker: DigisosSoker?,
+        val tilleggsinformasjon: Tilleggsinformasjon?
+)
+
+data class OriginalSoknadNAV(
+        val navEksternRefId: String,
+        val metadata: String,
+        val vedleggMetadata: String,
+        val soknadDokument: DokumentInfo,
+        val vedlegg: List<DokumentInfo>,
+        val timestampSendt: Long
+)
+
+data class DokumentInfo(
+        val filnavn: String,
+        val dokumentlagerDokumentId: String,
+        val storrelse: Long
+)
+
+data class EttersendtInfoNAV(
+        val ettersendelser: List<Ettersendelse>
+)
+
+data class Ettersendelse(
+        val navEksternRefId: String,
+        val vedleggMetadata: String,
+        val vedlegg: List<DokumentInfo>,
+        val timestampSendt: Long
+)
+
+data class DigisosSoker(
+        val metadata: String,
+        val dokumenter: List<DokumentInfo>,
+        val timestampSistOppdatert: Long
+)
+
+data class Tilleggsinformasjon(
+        val enhetsnummer: String?
+)
+
+data class KommuneInfo(
+        val kommunenummer: String,
+        val kanMottaSoknader: Boolean,
+        val kanOppdatereStatus: Boolean,
+        val harMidlertidigDeaktivertMottak: Boolean,
+        val harMidlertidigDeaktivertOppdateringer: Boolean,
+        val kontaktPersoner: Kontaktpersoner?,
+        val harNksTilgang: Boolean,
+        val behandlingsansvarlig: String?
+)
+
+data class Kontaktpersoner(
+        val fagansvarligEpost: List<String>,
+        val tekniskAnsvarligEpost: List<String>
+)
+
+data class ErrorMessage(
+        val error: String?,
+        val errorCode: Any?,
+        val errorId: String?,
+        val errorJson: Any?,
+        val message: String?,
+        val originalPath: String?,
+        val path: String?,
+        val status: Int?,
+        val timestamp: Long?
+)
