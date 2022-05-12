2fac7a79c44d8f793776e875fcfa4d1f6cc562b7
martintveter <41987225+martintveter@users.noreply.github.com>
2022-05-12 13:28:32 +0200

    snyk: bump spring (#64)

--- sosialhjelp-common-metrics/build.gradle.kts
+++ sosialhjelp-common-metrics/build.gradle.kts
@@ -2,7 +2,7 @@ object Versions {
     const val slf4j = "1.7.36"
     const val micrometer = "1.8.5"
     const val json = "20220320"
-    const val spring = "5.3.19"
+    const val spring = "5.3.20"
     const val commonsLang3 = "3.12.0"
     const val aspectj = "1.9.9.1"
 }
