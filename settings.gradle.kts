3c602559b02e2e9e4b9e95cb93592eeecb8abe97
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-06-29 13:55:24 +0200

    fjerner client-utils modulen. Blir ikke brukt noe sted

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,4 @@ rootProject.name = "sosialhjelp-common"
 include("sosialhjelp-common-kotlin-utils")
 include("sosialhjelp-common-selftest")
 include("sosialhjelp-common-api")
-include("sosialhjelp-common-client-utils")
 include("sosialhjelp-common-metrics")
--- sosialhjelp-common-client-utils/src/main/kotlin/no/nav/sosialhjelp/client/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.sosialhjelp.client.utils
-
-object Constants {
-    const val BEARER = "Bearer "
-}
