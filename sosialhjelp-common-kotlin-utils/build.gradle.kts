8d4249c7dcc853c6f77b8985fc44cb23506120cd
Amund Sørumshagen <3726993+amundsor@users.noreply.github.com>
2023-09-18 19:53:57 +0200

    Security updates (#84)
    * Upgrade Apache FOP
    * Constraints org.apache.commons:commons-compress

--- sosialhjelp-common-kotlin-utils/build.gradle.kts
+++ sosialhjelp-common-kotlin-utils/build.gradle.kts
@@ -1,6 +1,7 @@
 object Versions {
     const val coroutines = "1.6.4"
     const val slf4j = "1.7.36"
+    const val commons = "1.24.0"
 }
 
 plugins {
@@ -19,7 +20,7 @@ dependencies {
     implementation("org.apache.pdfbox:pdfbox:2.0.27")
     implementation("org.apache.poi:poi:5.2.3")
     implementation("org.apache.poi:poi-ooxml:5.2.3")
-    implementation("org.apache.xmlgraphics:fop:2.8")
+    implementation("org.apache.xmlgraphics:fop:2.9")
 
     implementation("org.docx4j:docx4j-JAXB-MOXy:11.4.9")
     implementation("org.docx4j:docx4j-JAXB-ReferenceImpl:11.4.9")
@@ -27,6 +28,12 @@ dependencies {
     implementation("org.apache.commons:commons-csv:1.10.0")
 
     testImplementation("org.assertj:assertj-core:3.23.1")
+
+    constraints {
+        implementation("org.apache.commons:commons-compress:1.24.0") {
+            because("https://github.com/advisories/GHSA-cgwf-w82q-5jrr")
+        }
+    }
 }
 
 publishing {
