f50db8a58d9d932a9e97092e1ddfdf038fbde356
hildenok <hilde.nokland@nav.no>
2023-11-28 10:30:29 +0100

    Fiks for å kunne kjøre tester på mac

--- sosialhjelp-common-kotlin-utils/src/main/kotlin/no/nav/sosialhjelp/kotlin/utils/pdf/filkonvertering/word/WordToPdfConverter.kt
+++ sosialhjelp-common-kotlin-utils/src/main/kotlin/no/nav/sosialhjelp/kotlin/utils/pdf/filkonvertering/word/WordToPdfConverter.kt
@@ -3,6 +3,7 @@ package no.nav.sosialhjelp.kotlin.utils.pdf.filkonvertering.word
 import no.nav.sosialhjelp.kotlin.utils.pdf.filkonvertering.FilTilPdfConverter
 import no.nav.sosialhjelp.kotlin.utils.pdf.filkonvertering.exception.WordKonverteringException
 import org.docx4j.Docx4J
+import org.docx4j.fonts.PhysicalFonts
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
@@ -14,6 +15,10 @@ object WordToPdfConverter : FilTilPdfConverter {
                 WordprocessingMLPackage.load(it)
             }
             return ByteArrayOutputStream().use {
+                // For å unngå feil med font på Mac slik at tester kan kjøre lokalt
+                if (System.getProperty("os.name").equals("Mac OS X", ignoreCase = true)) {
+                    PhysicalFonts.setRegex(".*(Courier New|Arial|Times New Roman|Comic Sans|Georgia|Impact|Lucida Console|Lucida Sans Unicode|Palatino Linotype|Tahoma|Trebuchet|Verdana|Symbol|Webdings|Wingdings|MS Sans Serif|MS Serif).*")
+                }
                 Docx4J.toPDF(wordFile, it)
                 it.flush()
                 it.toByteArray()
